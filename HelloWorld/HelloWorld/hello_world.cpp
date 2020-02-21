aa04e259ac3db27eb708dcf4c8d5596ef56bbbb2
Shaun Chemplavil <schemp98@hotmail.com>
2020-02-21 16:38:45 -0600

    forgot a file...

---
+++ HelloWorld/HelloWorld/hello_world.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main()
+{
+    //  i do think that /*
+    // or */ will work okay
+    std::cout << "Hello\nWorld!" << std::endl; 
+    //std::cout << "This (\") is a quote, and this (\\) is a backslash" << std::endl;
+}
