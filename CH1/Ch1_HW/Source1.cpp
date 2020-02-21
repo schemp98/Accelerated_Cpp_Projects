295d8973ea54819441e692ed4725a671c95ba32f
Shaun Chemplavil <schemp98@hotmail.com>
2020-02-21 16:36:37 -0600

    Creating Repo for Accelerated C++ HW

---
+++ CH1/Ch1_HW/Source1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string s = "What is your name?";
+	std::cout << s << std::endl;
+	std::string name;
+	std::cin >> name;
+	std::cout << name;
+
+}
